7ca8f2c67825cf6017602115aef024ebc296e283
Hugo Feliciano <hugo.feliciano666@gmail.com>
2019-11-30 19:29:01 +0000

    Add TitleCrew and TitleEpisode structs

---
+++ LP2_P1/TitleCrew.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LP2_P1
+{
+    public struct TitleCrew
+    {
+        public string Tconst { get; }
+        public string Directors { get; }
+        public string Writers { get; }
+
+        public TitleCrew(string tconst, string directors, string writers)
+        {
+            Tconst = tconst;
+            Directors = directors;
+            Writers = writers;
+        }
+    }
+}
---
+++ LP2_P1/TitleEpisode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LP2_P1
+{
+    public struct TitleEpisode
+    {
+        public string Tconst { get; }
+        public string ParentTconst { get; }
+        public int? SeasonNumber { get; }
+        public int? EpisodeNumber { get; }
+
+        public TitleEpisode(string tconst, string parentTconst,
+            int? seasonNumber = null, int? episodeNumber = null)
+        {
+            Tconst = tconst;
+            ParentTconst = parentTconst;
+            SeasonNumber = seasonNumber;
+            EpisodeNumber = episodeNumber;
+        }
+    }
+}
