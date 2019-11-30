6211248a9b43a2dd7796e92e92de8e8da4ef8a72
PmaiWoW <PmaiWoW@Gmail.com>
2019-11-30 18:51:53 +0000

    Add initial NameBasics struct to serve as reference

---
+++ LP2_P1/NameBasics.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LP2_P1
+{
+    public struct NameBasics
+    {
+        public string NConst { get; }
+        public string PrimaryName { get; }
+        public int BirthYear { get; }
+        public int? DeathYear { get; }
+        public string[] PrimaryProfession { get; }
+        public string[] KnownForTitles { get; }
+
+        public NameBasics(string nConst, string primaryName, int birthYear,
+            string[] primaryProfession, string[] knownForTitles,
+            int? deathYear = null)
+        {
+            NConst = nConst;
+            PrimaryName = primaryName;
+            BirthYear = birthYear;
+            DeathYear = deathYear;
+            PrimaryProfession = primaryProfession;
+            KnownForTitles = knownForTitles;
+        }
+    }
+}
